7d06d0bcce84a52c92acb1645543a9532e3f6383
Chris Bubel <christopher.s.bubel@gmail.com>
2017-11-28 21:13:59 -0500

    Updated HTML IDs and added output elements

--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,20 @@ import './styles/reset.css';
 import './styles/main.css';
 
 // Members
-const INPUT = document.getElementById('game-input') as HTMLInputElement;
+const INPUT = document.getElementById('input') as HTMLInputElement;
+const OUTPUT = document.getElementById('output');
 
 // Functions
 function handleInput(value: string): void {
-    console.log(value);
+    OUTPUT.appendChild(newOutput(value, 'output-msg', 'output-msg-player'));
+}
+
+function newOutput(value: string, ...classes: string[]): HTMLElement {
+    const node = document.createElement('li');
+    node.classList.add(...classes);
+    node.innerText = value;
+
+    return node;
 }
 
 // Attach listeners
